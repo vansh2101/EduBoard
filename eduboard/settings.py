496b3dd9bd9ef245dae7c3ee4ec99ec2a99a20c9
VANSH  SACHDEVA <68803315+vansh2101@users.noreply.github.com>
2021-10-10 14:41:20 +0530

    Update settings.py

--- eduboard/settings.py
+++ eduboard/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'eduboard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'eduboard',
-        'USER': 'postgres',
-        'PASSWORD': 'vansh2101',
-        'HOST': 'localhost'
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': ''
     }
 }
 
